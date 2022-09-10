450d342ef5702cbcb70c231e4c7caca1edcbbfbe
dominikcsiszer <dominikcsiszer@icloud.com>
2022-09-10 16:28:29 +0200

    lampAction()

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 $(() => {
-    //Kezdés()
+    loadLamp(9)
+    lampAction()
 })
+
 
 const LAMPAK = []
 function loadLamp(n) {
-    
+
+}
+
+function lampAction() {
+    LAMPAK.forEach(element => {
+        element.on("click", () =>{
+            console.log(this)
+        })
+    })
+
+    // $(".lampa").on("click", () => {
+    //     console.log(this)
+    //     $(this).toggleClass("active")
+    // })
 }
