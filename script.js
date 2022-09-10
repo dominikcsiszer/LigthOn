c1b16de41ecfd9f34b594da0b8b6df820745a64e
dominikcsiszer <dominikcsiszer@icloud.com>
2022-09-10 17:19:18 +0200

    Random lámpa fel/le

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-
 $(() => {
-    loadLamp(9)
-    lampAction()
+    loadLamp($("input[type=range]").val())
 })
 
-const LAMPAK = []
+let LAMPAK = []
 function loadLamp(n) {
+    LAMPAK = []
     html ="";
     for (let index = 0; index < n; index++) {
-        html+="<div id='"+index+"' class='lampa'></div>";
+        LAMPAK.push("<div class='lampa'></div>")
     }
-    $(".container").html(html);
-    LAMPAK.push($(".lampa"));
+
+    let random = Math.floor(Math.random() * n/2)+1
+    for (let index = 0; index < random; index++) {
+        let randomIndex = Math.floor(Math.random() * LAMPAK.length)+1
+        LAMPAK[randomIndex] = "<div class='lampa active'></div>"
+    }
+
+    $(".container").html(LAMPAK);
+    lampAction()
 }
 
 function lampAction() {
-    $(".lampa").on("click", function(){
+    $(".lampa").on("click", function() {
         $(this).toggleClass("active")
+    })
+
+    $("input[type=range]").change(function() {
+        loadLamp($("input[type=range]").val())
     })
 }
